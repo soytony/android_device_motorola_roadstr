0934c9d9df330b51a82ef9d713904bcb3670db2f
tonyfan <waterwet@outlook.com>
2026-08-14 00:43:33 +0800

    fix(roadstr): add sepolicy ignores for CQATest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ BOARD_AVB_ROLLBACK_INDEX := 1
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/system_ext/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/system_ext/private
+BOARD_SYSTEM_EXT_SEPOLICY_PREBUILT_DIRS += $(DEVICE_PATH)/sepolicy/system_ext
 
 # Inherit from the proprietary version
 -include vendor/motorola/roadstr/BoardConfigVendor.mk
