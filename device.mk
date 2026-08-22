3b299570498dff8304a93fe559b0f990851978aa
tonyfan <waterwet@outlook.com>
2026-08-22 13:37:50 +0800

    fix: move tinyxml exclusion to common guard

--- device.mk
+++ device.mk
@@ -125,7 +125,3 @@ PRODUCT_COPY_FILES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/motorola/roadstr/roadstr-vendor.mk)
-
-# Keep stock tinyxml private to poweropt; do not install it as the global
-# /vendor/lib64/libtinyxml2.so, which belongs to the common source build.
-PRODUCT_PACKAGES -= libtinyxml2_vendor
